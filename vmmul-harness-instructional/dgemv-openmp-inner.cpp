ca6ae00a7d5076f15256ccfea07d6326d276f1aa
jasonnguyen717 <33739106+jasonnguyen717@users.noreply.github.com>
2022-11-30 20:18:36 -0800

    Update dgemv-openmp-inner.cpp

--- vmmul-harness-instructional/dgemv-openmp-inner.cpp
+++ vmmul-harness-instructional/dgemv-openmp-inner.cpp
@@ -26,7 +26,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // nthreads and thread_id so as to not taint your timings
    
    for(int i = 0; i < n; i++) {
-      #pragma omp parallel for    
+      #pragma omp parallel    
       for(int j = 0; j < n; j++)
       {
          y[i] += A[i * n + j] * x[j];
